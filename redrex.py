1047664cb71f59a0874340a1a1ecd2eea63657a0
rhshimul2727 <92758735+rhshimul2727@users.noreply.github.com>
2021-10-25 06:01:07 +0600

    Update redrex.py

--- redrex.py
+++ redrex.py
@@ -1,7 +1,7 @@
 import smtplib
 server = smtplib.SMTP("smtp.gmail.com",587)
 server.starttls()
-server.login("doubleaech1@gmail.com","11223344$")
+server.login("gabiroy506@gmail.com","701702703")
 for i in range(5000):
     server.sendmail("choda@gmail.com","doubleaech3@gmail.com","You have been crash in 10 second")
     print 'massage has been send'
